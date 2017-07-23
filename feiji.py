db0e35ee1d79826f8c78f8ce75f6c16d705246a5
yuzhiyi <81784779@qq.com>
2017-07-23 10:13:52 +0800

    add hero images

--- feiji.py
+++ feiji.py
@@ -2,23 +2,32 @@
 #auth:zhiyi
 import pygame
 import time
+import sys
 
 def main():
     #整体流程控制
     #  创建一个窗口显示东西
-    screen = pygame.display.set_mode((488,852),0,32)
+    weight = 400
+    height = 600
+    screen = pygame.display.set_mode((weight,height),0,32)
 
     #  创建一个背景图片
     background = pygame.image.load('./resource/background.png')
+    #  创建一个玩家飞机图片
+    hero = pygame.image.load('./resource/hero1.png')
 
-    #  3.将北京图片粘贴到窗口中
+    #  3.将背景图片粘贴到窗口中
     screen.blit(background,(0,0))
-
+    #  将飞机图片粘贴到窗口中
+    screen.blit(hero,(weight/2-(100/2),height-150))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
         #  4.显示窗口中的内容
         pygame.display.update()
 
-        time.sleep(0.04)
+        time.sleep(0.05)
 
 
 if __name__ == '__main__':
